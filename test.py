e07a9705220dbfc225937f75cd72ac14d52c77e0
rxfix <panter2004@gmail.com>
2021-05-07 18:49:54 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+class Auto:
+    # атрибуты класса
+    auto_name = "Lexus"
+    auto_model = "RX 350L"
+    auto_year = 2019
+
+    # методы класса
+    def on_auto_start(self):
+        print(f"Заводим автомобиль")
+
+    def on_auto_stop(self):
+        print("Останавливаем работу двигателя")
