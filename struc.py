0fdcd5ad6dab012b3c2d09cf10926c5952b2ad10
rxfix <panter2004@gmail.com>
2021-05-26 11:36:29 +0300

    Merge remote-tracking branch 'origin/lessone-7' into lessone-7
    # Conflicts:
    #	test.py

---
+++ struc.py
@@ -0,0 +1,45 @@
+from pprint import pprint
+
+struc_dic = {'my_project':
+                 [{'settings':
+                       ['__init__.py',
+                        'dev.py',
+                        'prod.py']},
+                  {'mainapp':
+                       ['__init__.py',
+                        'models.py',
+                        'views.py',
+                        {'templates':
+                             [{'mainapp':
+                                 ['base.html',
+                                  'index.html']}]}]},
+                  {'authapp':
+                       ['__init__.py',
+                        'models.py',
+                        'views.py',
+                               {'templates':
+                                    [{'authapp':
+                                        ['base.html',
+                                         'index.html']}]}]}]}
+pprint(struc_dic)
+# my_project
+#    |--settings
+#    |  |--__init__.py
+#    |  |--dev.py
+#    |  |--prod.py
+#    |--mainapp
+#    |  |--__init__.py
+#    |  |--models.py
+#    |  |--views.py
+#    |  |--templates
+#    |     |--mainapp
+#    |        |--base.html
+#    |        |--index.html
+#    |--authapp
+#    |  |--__init__.py
+#    |  |--models.py
+#    |  |--views.py
+#    |  |--templates
+#    |     |--authapp
+#    |        |--base.html
+#    |        |--index.html
--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-<<<<<<<<< Temporary merge branch 1
-ццц
-=========
+
 import os
 
 root_dir = './'
@@ -35,4 +33,110 @@ __step__ = 3
 ROOT = os.path.abspath(os.path.join(__file__, '..'))
 print(ROOT)
 print(os.listdir(ROOT))
->>>>>>>>> Temporary merge branch 2
+
+folder = r'C:\Users\user\AppData\Local\Programs\Python\Python39\Lib\site-packages\django'
+django_dirs = [item
+               for item in os.listdir(folder)
+               if os.path.isdir(os.path.join(folder, item))]
+print(django_dirs)
+
+__создание_1000_файлов__ = 4
+# import os
+# import random
+#
+# folder = 'some_data'
+# letters = [chr(code) for code in range(ord('a'), ord('z') + 1)]
+# for _ in range(10 ** 3):
+#    f_name = ''.join(random.sample(letters, random.randint(5, 10)))
+#    f_content = bytes(random.randint(0, 255)
+#                      for _ in range(random.randrange(10 ** 5)))
+#    with open(os.path.join(folder, f'{f_name}.bin'), 'wb') as f:
+#        f.write(f_content)
+
+__Подсчет_файлов__ = 5
+# import os
+# from time import perf_counter
+#
+# folder = 'some_data'
+# start = perf_counter()
+# size_threshold = 15 * 2 ** 10
+# small_files = [item
+#               for item in os.listdir(folder)
+#               if os.stat(os.path.join(folder, item)).st_size < size_threshold]
+# print(len(small_files), perf_counter() - start)
+# # 155 2.271335837
+# start = perf_counter()
+# small_files_2 = [item.name
+#                  for item in os.scandir(folder)
+#                  if item.stat().st_size < size_threshold]
+# print(len(small_files_2), perf_counter() - start)
+# print(small_files == small_files_2)
+#
+#
+# import os
+# from time import perf_counter
+#
+# folder = 'some_data'
+# start = perf_counter()
+# all_files = [item
+#               for item in os.listdir(folder)]
+# print(len(all_files), perf_counter() - start)
+# # 1000 0.056053622000000025
+# start = perf_counter()
+# all_files_2 = [item.name
+#                 for item in os.scandir(folder)]
+# print(len(all_files_2), perf_counter() - start)
+# print(all_files == all_files_2)
+
+
+__методы_и_пути__ = 6
+
+__создание_переименование_и_удаление_папок__ = 7
+
+import os
+
+dir_name = 'sample_dir'
+if not os.path.exists(dir_name):
+   os.mkdir(dir_name)
+
+import os
+
+dir_path = os.path.join('data', 'src')
+if not os.path.exists(dir_path):
+   os.makedirs(dir_path)
+
+dir_path = os.path.join('data', '555')
+if not os.path.exists(dir_path):
+   os.makedirs(dir_path)
+   print(dir_path)
+
+
+
+import yaml
+from pprint import pprint
+
+with open('info.yaml') as f:
+    templates = yaml.safe_load(f)
+
+print(templates)
+
+import yaml
+
+trunk_template = [
+    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
+    'switchport trunk native vlan 999', 'switchport trunk allowed vlan'
+]
+
+access_template = [
+    'switchport mode access', 'switchport access vlan',
+    'switchport nonegotiate', 'spanning-tree portfast',
+    'spanning-tree bpduguard enable'
+]
+
+to_yaml = {'trunk': trunk_template, 'access': access_template}
+
+with open('sw_templates.yaml', 'w') as f:
+    yaml.dump(to_yaml, f, default_flow_style=False)
+
+with open('sw_templates.yaml') as f:
+    print(f.read())
